77424e8655c6eaf057a3a1be9af0bbe55d642c0d
HolyWalley <yasha.krasnov@gmail.com>
2021-03-26 22:01:25 +0300

    implement net_http adapter

--- lib/sniffer/adapters/net_http_adapter.rb
+++ lib/sniffer/adapters/net_http_adapter.rb
@@ -1,50 +1,60 @@
 # frozen_string_literal: true
 
-require 'net/http'
-require 'benchmark'
+require "net/http"
+require "benchmark"
 
 module Sniffer
   module Adapters
-    # Net::HTTP adapter
-    module NetHttpAdapter
-      def self.included(base)
-        base.class_eval do
-          alias_method :request_without_sniffer, :request
-          alias_method :request, :request_with_sniffer
-        end
+    class NetHttpAdapter
+      def initialize(handler)
+        @handler = handler
       end
 
+      def sniff
+        Net::HTTP.define_method(:request_with_sniffer, build_request_with_sniffer(@handler))
+        Net::HTTP.alias_method(:request_without_sniffer, :request)
+        Net::HTTP.alias_method(:request, :request_with_sniffer)
+
+        yield
+      ensure
+        Net::HTTP.alias_method(:request, :request_without_sniffer)
+        Net::HTTP.remove_method(:request_with_sniffer)
+        Net::HTTP.remove_method(:request_without_sniffer)
+      end
+
+      private
+
       # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
-      def request_with_sniffer(req, body = nil, &block)
-        if started? && Sniffer.enabled?
-          data_item = Sniffer::DataItem.new
-          data_item.request = Sniffer::DataItem::Request.new(host: @address,
-                                                             method: req.method,
-                                                             query: req.path,
-                                                             port: @port,
-                                                             headers: req.each_header.collect.to_h,
-                                                             body: req.body.to_s)
+      def build_request_with_sniffer(handler)
+        lambda do |req, body = nil, &block|
+          data = {
+            request: {
+              host: @address,
+              method: req.method,
+              query: req.path,
+              port: @port,
+              headers: req.each_header.collect.to_h,
+              body: req.body.to_s
+            }
+          }
 
-          Sniffer.store(data_item)
+          bm = Benchmark.realtime do
+            @response = request_without_sniffer(req, body, &block)
+          end
+
+          data[:response] = {
+            status: @response.code.to_i,
+            headers: @response.each_header.collect.to_h,
+            body: @response.body.to_s,
+            timing: bm
+          }
+
+          handler.handle(data)
+
+          @response
         end
-
-        bm = Benchmark.realtime do
-          @response = request_without_sniffer(req, body, &block)
-        end
-
-        if started? && Sniffer.enabled?
-          data_item.response = Sniffer::DataItem::Response.new(status: @response.code.to_i,
-                                                               headers: @response.each_header.collect.to_h,
-                                                               body: @response.body.to_s,
-                                                               timing: bm)
-
-          Sniffer.notify_response(data_item)
-        end
-
-        @response
       end
       # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
     end
   end
 end
-Net::HTTP.include Sniffer::Adapters::NetHttpAdapter
