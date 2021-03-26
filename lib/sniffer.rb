c3f9ac168c0480ffab8e1b1f6ccf0892aca950f8
HolyWalley <yasha.krasnov@gmail.com>
2021-03-26 22:01:36 +0300

    implement little sniffer

--- lib/sniffer.rb
+++ lib/sniffer.rb
@@ -1,11 +1,41 @@
 # frozen_string_literal: true
 
-require "logger"
-
 require_relative "sniffer/version"
+require_relative "sniffer/adapters/net_http_adapter"
 
 # Sniffer allows to log http requests
 module Sniffer
-end
+  # LittleSniffer allows to log http request locally
+  class LittleSniffer
+    UnsupportedAdapterError = Class.new(StandardError)
+    HandlerDoesNotMatchTheInterfaceError = Class.new(StandardError)
 
-require_relative "sniffer/adapters/net_http_adapter"
+    ADAPTERS_MAP = {
+      net_http: Adapters::NetHttpAdapter
+    }.freeze
+
+    def initialize(handler, adapter = :net_http, &block)
+      adapter = validate_adapter(adapter)
+      validate_handler(handler)
+
+      adapter.new(handler).sniff(&block)
+    end
+
+    private
+
+    def validate_handler(handler)
+      return if handler.respond_to?(:handle)
+
+      raise HandlerDoesNotMatchTheInterfaceError
+    end
+
+    def validate_adapter(adapter)
+      adapter = ADAPTERS_MAP[adapter]
+
+      return adapter if adapter
+
+      error_message = "maybe you have misspelled it. Appropriate types are: #{ADAPTERS_MAP.keys.join(', ')}"
+      raise UnsupportedAdapterError, error_message
+    end
+  end
+end
